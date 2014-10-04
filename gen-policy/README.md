b544c7ec7c0ee1f49359b74fa6778f46ae80ea8e
Andy Leung <andyleung@gmail.com>
2014-10-04 12:28:31 +0800

    Auto-gen srx policy

---
+++ gen-policy/README.md
@@ -0,0 +1,3 @@
+
+Using the Python and jinja2 library to general X number of SRX policies. 
+
---
+++ gen-policy/policy.py
@@ -0,0 +1,37 @@
+import os
+import jinja2
+import webapp2
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        self.render("input_config.html")
+
+    def post(self):
+	num_of_policy = self.request.get("num_of_policy")
+	zone_A = self.request.get("zone_A")
+	zone_B = self.request.get("zone_B")
+	params = dict(num_of_policy = num_of_policy,
+			zone_A = zone_A,
+			zone_B = zone_B)
+	if not (num_of_policy and zone_A and zone_B):
+        	self.render("input_config.html",**params)
+	else:
+        	self.render("output_config.html",**params)
+
+app = webapp2.WSGIApplication([('/',MainPage),
+                              ],
+                              debug=True)
